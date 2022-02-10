0c66e9f981134ec72d1949bd5f1ce693f3694f81
webprofusion-chrisc <christopher.cook@webprofusion.com>
2022-02-10 16:30:01 +0800

    Output build to docs for publish

---
+++ docs/precache-manifest.b043ac9ec20744953fbfeedc7954defe.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1997a6d93f8e07a18825d0e91bd22e20",
+    "url": "/CNAME"
+  },
+  {
+    "revision": "bf09b6f1326d789e7d88",
+    "url": "/css/app.6070f056.css"
+  },
+  {
+    "revision": "e4ea1223cdecb6fd100a",
+    "url": "/css/chunk-vendors.5d3035bc.css"
+  },
+  {
+    "revision": "5261a8d68222698122bba0837f83ab68",
+    "url": "/icon.png"
+  },
+  {
+    "revision": "d891a6e762d277dd7e315e874f01159d",
+    "url": "/img/logo.d891a6e7.png"
+  },
+  {
+    "revision": "0f6ed316d69e672e6e2860784d290415",
+    "url": "/index.html"
+  },
+  {
+    "revision": "d51a6147800b541c81af",
+    "url": "/js/about.9858626b.js"
+  },
+  {
+    "revision": "bf09b6f1326d789e7d88",
+    "url": "/js/app.f9906adb.js"
+  },
+  {
+    "revision": "e4ea1223cdecb6fd100a",
+    "url": "/js/chunk-vendors.4b792e69.js"
+  },
+  {
+    "revision": "9928a13f9a7e0c2328ee0ab07a7a7e18",
+    "url": "/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/robots.txt"
+  }
+]);
